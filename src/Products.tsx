92a2e797db84e72a81da7457b5da5a9327b446c7
arif mahmud <mahmudarif1175@gmail.com>
2023-09-14 17:14:31 +0600

    added prodcut list card

--- src/Products.tsx
+++ src/Products.tsx
@@ -1,24 +1,64 @@
-import { gql, useQuery } from '@apollo/client';
-import Statistic from 'antd/es/statistic/Statistic';
-import React from 'react';
+import { gql, useQuery } from "@apollo/client"
+import { Statistic, Card, Spin, Row, Col } from "antd"
+import React from "react"
 
-const countQuery = gql`
-  query CountQuery {
-    products_aggregate {
-      aggregate {
-        count
-      }
-    }
-  }
+interface Product {
+	id: number
+	name: string
+	description: string
+	price: number
+	stock: number
+}
+
+const GET_PRODUCTS = gql`
+	query GetProducts {
+		products {
+			id
+			name
+			description
+			price
+			stock
+		}
+		products_aggregate {
+			aggregate {
+				count
+			}
+		}
+	}
 `
 
 export function Products(): JSX.Element {
+	const { loading, error, data } = useQuery(GET_PRODUCTS)
 
-  const { data } = useQuery(countQuery);
+	if (loading) {
+		return <Spin size='large' />
+	}
 
-  return (
-    <div>
-      <Statistic title="Total products" value={data?.products_aggregate?.aggregate.count} />
-    </div>
-  );
+	if (error) {
+		console.error("Error fetching data:", error)
+		return <div>Error fetching data</div>
+	}
+
+	const productData: Product[] = data.products
+
+	return (
+		<div>
+			<h2>Available Products</h2>
+
+			<Row gutter={16} style={{ display: "flex", flexWrap: "wrap" }}>
+				{productData.map((product: Product) => (
+					<Col span={8} key={product.id}>
+						<div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+							<Card title={product.name} style={{ flex: "1", marginBottom: "16px" }}>
+								<p>{product.description}</p>
+								<p>Price: ${product.price}</p>
+								<p>In-Stock: {product.stock}</p>
+							</Card>
+						</div>
+					</Col>
+				))}
+			</Row>
+			<Statistic title='Total products' value={data?.products_aggregate?.aggregate.count} />
+		</div>
+	)
 }
